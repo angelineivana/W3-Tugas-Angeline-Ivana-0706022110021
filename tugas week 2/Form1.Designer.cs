4f64b259656be90bf1059255fec02fbde762366c
angelineivana <angelineivana01@student.ciputra.ac.id>
2022-03-14 19:50:39 +0700

    Add project files.

---
+++ tugas week 2/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace tugas_week_2
+{
+    partial class FormPengaturanWarna
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelInput = new System.Windows.Forms.Label();
+            this.TextBoxInput = new System.Windows.Forms.TextBox();
+            this.BtnProses = new System.Windows.Forms.Button();
+            this.LabelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LabelInput
+            // 
+            this.LabelInput.AutoSize = true;
+            this.LabelInput.Location = new System.Drawing.Point(20, 19);
+            this.LabelInput.Name = "LabelInput";
+            this.LabelInput.Size = new System.Drawing.Size(105, 25);
+            this.LabelInput.TabIndex = 0;
+            this.LabelInput.Text = "Input Data :";
+            // 
+            // TextBoxInput
+            // 
+            this.TextBoxInput.Location = new System.Drawing.Point(24, 47);
+            this.TextBoxInput.Name = "TextBoxInput";
+            this.TextBoxInput.Size = new System.Drawing.Size(456, 31);
+            this.TextBoxInput.TabIndex = 1;
+            this.TextBoxInput.TextChanged += new System.EventHandler(this.TextBoxInput_TextChanged);
+            // 
+            // BtnProses
+            // 
+            this.BtnProses.Location = new System.Drawing.Point(24, 84);
+            this.BtnProses.Name = "BtnProses";
+            this.BtnProses.Size = new System.Drawing.Size(112, 34);
+            this.BtnProses.TabIndex = 2;
+            this.BtnProses.Text = "Proses";
+            this.BtnProses.UseVisualStyleBackColor = true;
+            this.BtnProses.Click += new System.EventHandler(this.BtnProses_Click);
+            // 
+            // LabelOutput
+            // 
+            this.LabelOutput.AutoSize = true;
+            this.LabelOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LabelOutput.Location = new System.Drawing.Point(24, 160);
+            this.LabelOutput.Name = "LabelOutput";
+            this.LabelOutput.Size = new System.Drawing.Size(134, 36);
+            this.LabelOutput.TabIndex = 3;
+            this.LabelOutput.Text = "[EMPTY]";
+            // 
+            // FormPengaturanWarna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 246);
+            this.Controls.Add(this.LabelOutput);
+            this.Controls.Add(this.BtnProses);
+            this.Controls.Add(this.TextBoxInput);
+            this.Controls.Add(this.LabelInput);
+            this.Name = "FormPengaturanWarna";
+            this.Text = "Form Pengaturan Warna";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelInput;
+        private System.Windows.Forms.TextBox TextBoxInput;
+        private System.Windows.Forms.Button BtnProses;
+        private System.Windows.Forms.Label LabelOutput;
+    }
+}
+
---
+++ tugas week 2/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tugas_week_2
+{
+    public partial class FormPengaturanWarna : Form
+    {
+        public FormPengaturanWarna()
+        {
+            InitializeComponent();
+        }
+        string TextBoxIsi = "";
+        string TextBoxWarna = "";
+        string TextBoxIsiCut = "";
+        string TextBoxWarnaCut = "";
+        string TextBoxWarnaLower = "";
+        string TextBoxFullLower = "";
+        float fontSizeBaru = 0;
+        bool canGivenColor;
+        int cekRestart = 0;
+        private void TextBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            TextBoxIsiCut = "";
+            TextBoxFullLower = TextBoxInput.Text.ToLower();
+            if (TextBoxInput.Text.Length < 4)
+            {
+                TextBoxFullLower = "syntax";
+            }
+            if (TextBoxInput.Text.Length >= 4)
+            {
+                TextBoxIsiCut = TextBoxInput.Text.Substring(0, 4);
+                if (TextBoxIsiCut.ToLower() == "isi:")
+                {
+                    TextBoxIsi = TextBoxInput.Text.Substring(4);
+                    if (TextBoxIsi.StartsWith(" "))
+                    {
+                        TextBoxIsi = TextBoxIsi.Substring(1);
+                    }
+                    canGivenColor = true;
+                }
+                else if (TextBoxFullLower != "restart" && TextBoxWarnaCut.ToLower() != "warna:" && TextBoxFullLower != "shown" && TextBoxFullLower != "hide" && TextBoxFullLower != "besarkan" && TextBoxFullLower != "kecilkan")
+                {
+                    TextBoxFullLower = "syntax";
+                }
+                else if (cekRestart == 1 && (TextBoxFullLower == "restart" || TextBoxWarnaCut.ToLower() == "warna:" || TextBoxFullLower == "shown" || TextBoxFullLower == "hide" || TextBoxFullLower == "besarkan" || TextBoxFullLower == "kecilkan"))
+                {
+                    TextBoxFullLower = "restart";
+                }
+                else
+                {
+                    canGivenColor = true;
+                    TextBoxWarnaCut = "";
+                }
+            }
+            if (TextBoxInput.Text.Length >= 6)
+            {
+                TextBoxWarnaCut = "";
+                TextBoxWarnaCut = TextBoxInput.Text.Substring(0, 6);
+                if (TextBoxWarnaCut.ToLower() == "warna:")
+                {
+                    TextBoxWarna = TextBoxInput.Text.Substring(6);
+                    if (TextBoxWarna.StartsWith(" "))
+                    {
+                        TextBoxWarna = TextBoxWarna.Substring(1);
+                    }
+                    TextBoxWarnaLower = TextBoxWarna.ToLower();
+                    if (TextBoxWarnaLower != "default" && TextBoxWarnaLower != "merah" && TextBoxWarnaLower != "hijau" && TextBoxWarnaLower != "biru")
+                    {
+                        TextBoxFullLower = "syntax";
+                    }
+                    else if (cekRestart == 1 && (TextBoxWarnaLower == "default" || TextBoxWarnaLower == "merah" || TextBoxWarnaLower == "hijau" || TextBoxWarnaLower == "biru"))
+                    {
+                        TextBoxFullLower = "restart";
+                    }
+                    else 
+                    {
+                        canGivenColor = true;
+                        TextBoxWarnaCut = "";
+                    }
+                }
+                else if (TextBoxFullLower != "restart" && TextBoxIsiCut.ToLower() != "isi:" && TextBoxFullLower != "shown" && TextBoxFullLower != "hide" && TextBoxFullLower != "besarkan" && TextBoxFullLower != "kecilkan")
+                {
+                    TextBoxFullLower = "syntax"; 
+                }
+                else
+                {
+                    canGivenColor = true;
+                    TextBoxWarnaCut = "";
+                }
+            }
+        }
+        private void BtnProses_Click(object sender, EventArgs e)
+        {
+            if (canGivenColor == true)
+            {
+                if (TextBoxFullLower == "shown")
+                {
+                    LabelOutput.Show();
+                }
+                else if (TextBoxFullLower == "hide")
+                {
+                    LabelOutput.Hide();
+                }
+                else if (TextBoxFullLower == "besarkan")
+                {
+                    LabelOutput.AutoSize = true;
+                    fontSizeBaru = LabelOutput.Font.Size * 1.1f;
+                    LabelOutput.Font = new Font(LabelOutput.Font.FontFamily, fontSizeBaru);
+                }
+                else if (TextBoxFullLower == "kecilkan")
+                {
+                    LabelOutput.AutoSize = true;
+                    fontSizeBaru = LabelOutput.Font.Size / 1.1f;
+                    LabelOutput.Font = new Font(LabelOutput.Font.FontFamily, fontSizeBaru);
+                }
+                else if (TextBoxWarnaLower == "default")
+                {
+                    LabelOutput.ForeColor = Color.Black;
+                }
+                else if (TextBoxWarnaLower == "merah")
+                {
+                    LabelOutput.ForeColor = Color.Red;
+                }
+                else if (TextBoxWarnaLower == "hijau")
+                {
+                    LabelOutput.ForeColor = Color.Green;
+                }
+                else if (TextBoxWarnaLower == "biru")
+                {
+                    LabelOutput.ForeColor = Color.Blue;
+                }
+                LabelOutput.Text = TextBoxIsi;
+            }
+            if (TextBoxFullLower == "restart")
+            {
+                LabelOutput.Show();
+                TextBoxWarnaLower = "default";
+                LabelOutput.ForeColor = Color.Black;
+                LabelOutput.Text = "[EMPTY]";
+                TextBoxIsi = "[EMPTY]";
+                LabelOutput.Font = new Font(LabelOutput.Font.FontFamily, 15);
+                canGivenColor = false;
+                cekRestart = 1;
+            }
+            else if (TextBoxFullLower != "syntax")
+            {
+                canGivenColor = true;
+                cekRestart = 0;
+            }
+
+            if (TextBoxFullLower == "syntax")
+            {
+                MessageBox.Show("Syntax tidak terdaftar");
+                canGivenColor = false;
+            }
+            else if (TextBoxFullLower != "restart")
+            {
+                canGivenColor = true;
+                cekRestart = 0;
+            }
+        }
+    }
+}
